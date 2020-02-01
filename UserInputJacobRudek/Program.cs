c0003800787808e2c15a6b39415a770df3075d3e
rudek <rudek@DESKTOP-OOLQ00I>
2020-01-31 21:02:23 -0500

    Add project files.

---
+++ UserInputJacobRudek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserInputJacobRudek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi this is Jacob Rudek. Please enter your email address to join the mailing list." +
+                "Type your email address below and hit enter");
+
+            string useremail;
+
+            useremail = Console.ReadLine();
+
+            Console.WriteLine("You Entered:" + "  " + useremail);
+        }
+    }
+}
